494dad2e1ab428583e4e8efd36d7493c62e2d2c7
chuisu <choi.brandon.h@gmail.com>
2023-12-02 17:58:07 -0500

    added several variables to be stored in h5

---
+++ SYBIL_vsti/Other sources/sybil ver sine wave (pops)/PluginProcessor.h
@@ -0,0 +1,135 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include <juce_dsp/juce_dsp.h>
+#include <thread>
+#include <mutex>
+#include <tensorflow/core/public/session.h>
+#include <tensorflow/core/platform/env.h>
+#include "tensorflow/cc/saved_model/loader.h"
+//==============================================================================
+/**
+*/
+class SYBIL_vstiAudioProcessor  : public juce::AudioProcessor, public juce::AudioIODeviceCallback
+                            #if JucePlugin_Enable_ARA
+                             , public juce::AudioProcessorARAExtension
+                            #endif
+{
+public:
+
+    //==============================================================================
+    SYBIL_vstiAudioProcessor();
+    virtual ~SYBIL_vstiAudioProcessor() override;
+    //==============================================================================
+
+    void startSYBIL();
+    void stopSYBIL();
+
+    // void audioDeviceIOCallback(const float** inputChannelData, int numInputChannels, float** outputChannelData, int numOutputChannels, int numSamples);
+    void audioDeviceAboutToStart(juce::AudioIODevice* device) override;
+    void audioDeviceStopped() override;
+
+    float detectBPM(std::vector<float>& audioBuffer);
+    void detectBPMThreaded(std::vector<float> audioData);
+    std::vector<float> computeHPCPs(std::vector<float>& audioData);
+    float predictNote(const std::vector<float>& hpcpValues);
+
+    juce::StringArray getInputDeviceNames();
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    void loadTFModel(const std::string& modelPath);
+
+
+    juce::AudioBuffer<float> ringBuffer;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    //==============================================================================
+
+    juce::AudioProcessorValueTreeState::ParameterLayout
+        createParameterLayout();
+    juce::AudioProcessorValueTreeState SYBILValueTreeState {*this, nullptr, "Parameters", createParameterLayout()};
+
+
+private:
+    //==============================================================================
+    //juce::AudioDeviceManager audioDeviceManager;
+
+    juce::dsp::Oscillator<float> sineOsc;
+    juce::ADSR adsr;
+    juce::ADSR::Parameters adsrParams;
+
+    std::thread bpmThread; // Thread for BPM detection
+    std::mutex bpmMutex;   // Mutex for protecting shared data
+    std::condition_variable bpmCondVar;
+    bool shouldExit = false; // A flag to signal the thread to terminate
+    bool hasNewData = false;  // A flag to signal the thread that new data is available
+    std::vector<float> threadAudioData;  // Buffer for data that should be processed by the thread
+    float bpm = 120.0;
+    float currentBPM = 120.0;
+    int samplesPerSixteenth = 5112;
+    float* bpmPointer;
+    bool isPredicting = false;
+    bool bpmThreadShouldRun = true;
+
+    float originalFrequencyOfVoiceSample = 440.0;
+    juce::AudioBuffer<float> voiceAudioBuffer;
+    int voiceSampleIndex = 0;
+    float pitchShiftRatio = 1.0;
+    float lastPredictedFrequency = -1.0;  // Initialize to an invalid value
+    float frequencyTolerance = 0.1;  // Frequency tolerance; you may need to adjust this value
+
+    float predictedFrequency = 440.0;
+
+    juce::AudioBuffer<float> inputBuffer;
+
+    tensorflow::Session* session;
+    tensorflow::SavedModelBundle bundle;
+    tensorflow::Status status;
+    juce::File binaryLocation = juce::File::getSpecialLocation(juce::File::currentExecutableFile);
+    juce::File modelFile = binaryLocation.getSiblingFile("sybil_model/saved_model.pb");
+    juce::File modelDir = modelFile.getParentDirectory();
+
+    int bpmCounter = 0;
+    int hpcpCounter = 0;
+    int writePos = 0;
+    double sampleRate = 0.0;
+    const int bpmThreshold = 258;
+    juce::UndoManager undoManager;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SYBIL_vstiAudioProcessor)
+};
--- sybil_preprocessor.py
+++ sybil_preprocessor.py
@@ -8,13 +8,39 @@ import tensorflow as tf
 from keras.layers import Dense
 from spleeter.separator import Separator
 import os
-import numpy as np
 from pathlib import Path
 from tensorflow.keras.layers import Activation
 from tensorflow.keras import backend as K
 from tensorflow.keras.utils import get_custom_objects
+import h5py
 
-def sybil_preprocessor(file_for_preprocessing):
+def custom_loss(y_true, y_pred):
+    in_range_mask = tf.logical_and(y_pred >= 130, y_pred <= 1046.5)
+    out_of_range_mask = tf.logical_not(in_range_mask)
+
+    zero_mask = tf.equal(y_true, 0.0)
+    non_zero_mask = tf.logical_not(zero_mask)
+
+    zero_pred_mask = tf.equal(y_pred, 0.0)
+    non_zero_pred_mask = tf.logical_not(zero_pred_mask)
+
+    in_range_error = tf.square(y_true - y_pred) * tf.cast(in_range_mask, dtype=tf.float32)
+    out_of_range_error = tf.square(y_true - y_pred) * tf.cast(out_of_range_mask, dtype=tf.float32)
+
+    zero_error = tf.square(y_true - y_pred) * tf.cast(tf.logical_and(zero_mask, non_zero_pred_mask), dtype=tf.float32)
+    non_zero_error = tf.square(y_true - y_pred) * tf.cast(tf.logical_and(non_zero_mask, zero_pred_mask), dtype=tf.float32)
+
+    out_of_range_penalty = 1  # increase this value to apply a larger penalty for out-of-range predictions
+    zero_penalty = 0.001  # increase this value to apply a larger penalty for non-zero predictions when true value is zero
+    non_zero_penalty = 0.1  # increase this value to apply a larger penalty for zero predictions when true value is non-zero
+
+    out_of_range_error *= out_of_range_penalty
+    zero_error *= zero_penalty
+    non_zero_error *= non_zero_penalty
+
+    return tf.reduce_mean(in_range_error + out_of_range_error + zero_error + non_zero_error)
+
+def sybil_preprocessor(file_for_preprocessing, hdf5_filename):
     audio_file = file_for_preprocessing
     loader = es.MonoLoader(filename=audio_file)
     audio = loader()
@@ -36,21 +62,6 @@ def sybil_preprocessor(file_for_preprocessing):
               es.FrameGenerator(audio, frameSize=samples_per_sixteenth, hopSize=samples_per_sixteenth)]
 
     # So, we need exactly as many members in the final input data arrays and vocal data arrays as there are frames
-    """
-    # Calculate BeatsLoudness, Beatogram
-    beats_loudness_algo = es.BeatsLoudness()
-    loudness, loudness_band_ratio = beats_loudness_algo(audio)
-    print(loudness, loudness_band_ratio)
-    beatogram_algo = es.Beatogram()
-    beatogram = beatogram_algo(loudness, loudness_band_ratio)
-    print(beatogram)
-    
-    # Calculate meter
-    meter_algo = es.Meter()
-    meter = meter_algo(beatogram)
-    print(meter)
-    """
-
     # use spleeter to split the vocals and accompaniment
     def check_if_spleeted(track_name, output_dir):
         # Spleeter's standard output format
@@ -82,29 +93,22 @@ def sybil_preprocessor(file_for_preprocessing):
     # Vocal data collection (training data)
     # Per beat, detect note onset and durations, with each node being one sixteenth note
     pitch_algo = es.PitchYinFFT(frameSize=samples_per_sixteenth)
-    # Instantiate the energy detector
+
+    # Instantiate all the algos for both vocals and accompaniment
     energy_algo = es.Energy()
+    mfcc_algo = es.MFCC(numberCoefficients=13)
+    chords_detection = es.ChordsDetection()
+    window = es.Windowing(type='blackmanharris62')
+    spectrum = es.Spectrum()
+    spectral_peaks = es.SpectralPeaks(orderBy='magnitude')
+    hpcp = es.HPCP()
+
     # Declare the pitches per sixteenth variable
-    pitches_per_sixteenth = []
+    vocal_pitches = []
+    vocal_energy = []
+    vocal_mfcc = []
     vocal_frames = [frame for frame in
                     es.FrameGenerator(vocals, frameSize=samples_per_sixteenth, hopSize=samples_per_sixteenth)]
-
-    for vocal_frame in vocal_frames:
-        # Detect if there's something happening or not
-        energy = energy_algo(vocal_frame)
-        pitches, _ = pitch_algo(vocal_frame)
-        # Check if there is sound happening AND if the pitch is in Tammy's range (between C3 and C6, to be safe)
-        if energy > 10 and 130 < pitches < 1046.5:
-            pitches_per_sixteenth.append(pitches)
-            print("adding: " + str(pitches))
-        else:
-            pitches_per_sixteenth.append(0)
-            print("adding a zero")
-
-    print(pitches_per_sixteenth)
-
-    # Accompaniment data collection (input data) in much the same way as each frame is a sixteenth, and each pitch is
-    # assigned per sixteenth, we need to do the same with chords and other data
 
     # Load up the accompaniment file
     accompaniment_data_loader = es.MonoLoader(filename=output_file_path_accompaniment)
@@ -112,19 +116,41 @@ def sybil_preprocessor(file_for_preprocessing):
     accompaniment_frames = [frame for frame in
                             es.FrameGenerator(accompaniment, frameSize=samples_per_sixteenth,
                                               hopSize=samples_per_sixteenth)]
+    # Check for frame alignment
+    if len(vocal_frames) != len(accompaniment_frames):
+        raise ValueError("Frame mismatch: The number of vocal and accompaniment frames does not match.")
 
-    chords_detection = es.ChordsDetection()
-    window = es.Windowing(type='blackmanharris62')
-    spectrum = es.Spectrum()
-    spectral_peaks = es.SpectralPeaks(orderBy='magnitude')
-    hpcp = es.HPCP()
+    # vocal data collection
+    for vocal_frame in vocal_frames:
+        # Detect energy
+        current_frame_vocal_energy = energy_algo(vocal_frame)
+        vocal_energy.append(current_frame_vocal_energy)
+        # print("added energy: " + str(current_frame_vocal_energy))
+        pitches, _ = pitch_algo(vocal_frame)
+        # Check if there is sound happening AND if the pitch is in Tammy's range (between C3 and C6, to be safe)
+        if current_frame_vocal_energy > 10 and 130 < pitches < 1046.5:
+            vocal_pitches.append(pitches)
+            # print("adding pitch: " + str(pitches))
+        else:
+            vocal_pitches.append(0)
+            # print("adding a zero")
+        # Add vocal mfccs
+        mfcc_bands, mfcc_coeffs = mfcc_algo(vocal_frame)
+        vocal_mfcc.append(mfcc_coeffs)
+        # print("adding MFCCs: " + str(mfcc_coeffs))
+    # print(vocal_energy)
+    # print(vocal_pitches)
+    # print(vocal_mfcc)
 
+    # Accompaniment data collection
+    # (input data) in much the same way as each frame is a sixteenth, and each pitch is
+    # assigned per sixteenth, we need to do the same with chords and other data
     # declare the variables that will be appended to
     accompaniment_chords = []
     accompaniment_groove = []
     accompaniment_volume = []
     accompaniment_energy = []
-    accompaniment_frame_hpcps = []
+    accompaniment_hpcps = []
 
     # OKAY!! So here's the bit where we add more data.
     # This data must be sliced properly.
@@ -135,29 +161,28 @@ def sybil_preprocessor(file_for_preprocessing):
         accompaniment_frame_spectrum = spectrum(accompaniment_frame_windowed)
         frequencies, magnitudes = spectral_peaks(accompaniment_frame_spectrum)
         accompaniment_frame_hpcp = hpcp(frequencies, magnitudes)
+        accompaniment_frame_energy = energy_algo(accompaniment_frame)
 
-        accompaniment_frame_hpcps.append(accompaniment_frame_hpcp)
-
-    accompaniment_frame_hpcps = array(accompaniment_frame_hpcps)
-    chords, strengths = chords_detection(accompaniment_frame_hpcps)
+        accompaniment_hpcps.append(accompaniment_frame_hpcp)
+        accompaniment_energy.append(accompaniment_frame_energy)
 
     numpy.set_printoptions(threshold=sys.maxsize)
+    print("accompaniment hpcps: " + str(accompaniment_hpcps))
 
     # Here, we need to slice the data so that it makes sense going into tensorflow
-    vocal_data = numpy.array(pitches_per_sixteenth)
-    input_data = numpy.array(accompaniment_frame_hpcps)
+    # As of 11/13/2023, we no longer need this format of data.
+    # Instead, we are going to write our collected data to an hdf5 file.
+    # There should be a separate part for training the model
+    # (this is better for testing anyway)
+    # vocal_data = numpy.array(vocal_pitches)
+    # input_data = numpy.array(accompaniment_frame_hpcps)
 
     # we need to slice the vocal and input data such that they make sense
 
     # Return vocal data and input data
-    return vocal_data, input_data
-
+    # return vocal_data, input_data
 
 # Define the neural network architecture
-
-# Define custom loss function (this one isn't super ideal, it needs to better account for rests)
-from main import custom_loss
-
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(512, activation='relu', input_shape=(12,)),
     tf.keras.layers.Dense(256, activation='relu'),
@@ -180,7 +205,7 @@ model.compile(tf.keras.optimizers.Adam(learning_rate=.0005), loss=custom_loss)
 callback = tf.keras.callbacks.LearningRateScheduler(scheduler)'''
 
 # List audio directory
-audio_directory = '/home/b/Workspace/sybil/raw_audio/'
+audio_directory = '/home/b/Workspace/sybil-git/sybil/raw_audio/'
 
 # Iterate through audio directory
 for idx, filename in enumerate(os.listdir(audio_directory)):
@@ -189,8 +214,12 @@ for idx, filename in enumerate(os.listdir(audio_directory)):
         print(file_path)
         vocal_data, input_data = sybil_preprocessor(file_path)
 
+
+'''
+        # right now, we're just trying to write the db, so we don't need to include this in the loop
         model.fit(input_data, vocal_data, epochs=256) #, callbacks=[callback])
 
+        # we need to save the model as a savedmodel, not an h5
         model.save('sybil.h5')
         model = tf.keras.models.load_model('sybil.h5', custom_objects={'custom_loss': custom_loss})
 
@@ -198,7 +227,9 @@ for idx, filename in enumerate(os.listdir(audio_directory)):
 
 model = tf.keras.models.load_model('sybil.h5', custom_objects={'custom_loss': custom_loss})
 
-'''for input_hpcp in input_data:
+for input_hpcp in input_data:
     input_hpcp = np.expand_dims(input_hpcp, axis=0)
     prediction = model.predict(input_hpcp)
-    print(prediction)'''
+    print(prediction)
+
+'''
