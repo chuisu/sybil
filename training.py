47454e1726bd38e5bbdf43fc1177d24e21e8c51d
chuisu <choi.brandon.h@gmail.com>
2023-12-27 14:34:19 -0500

    cleanup commit

--- training.py
+++ training.py
@@ -5,7 +5,7 @@ from tensorflow.keras.layers import Input, Dense, LSTM, Concatenate
 from tensorflow.keras.models import Model
 
 
-with h5py.File('/mnt/data/sybil_features.h5', 'r') as hdf5_file:
+with h5py.File('sybil_features.h5', 'r') as hdf5_file:
     vocal_energy = np.array(hdf5_file['vocal_energy'])
     vocal_mfcc = np.array(hdf5_file['vocal_mfcc'])
     vocal_pitches = np.array(hdf5_file['vocal_pitches'])
@@ -14,6 +14,9 @@ with h5py.File('/mnt/data/sybil_features.h5', 'r') as hdf5_file:
 
 # Input layers
 input_accompaniment_hpcp = Input(shape=(accompaniment_hpcps.shape[1],))
+# Ensure accompaniment_energy is a 2D array, e.g., reshape if necessary
+if accompaniment_energy.ndim == 1:
+    accompaniment_energy = accompaniment_energy.reshape(-1, 1)
 input_accompaniment_energy = Input(shape=(accompaniment_energy.shape[1],))
 
 # Shared layers
@@ -21,7 +24,12 @@ shared = Concatenate()([input_accompaniment_hpcp, input_accompaniment_energy])
 shared = Dense(128, activation='relu')(shared)
 
 # Separate output layers for each target feature
+# Ensure accompaniment_energy is a 2D array, e.g., reshape if necessary
+if vocal_energy.ndim == 1:
+    vocal_energy = vocal_energy.reshape(-1, 1)
 output_vocal_energy = Dense(vocal_energy.shape[1], activation='linear', name='vocal_energy')(shared)
+if vocal_pitches.ndim == 1:
+    vocal_pitches = vocal_pitches.reshape(-1, 1)
 output_vocal_pitches = Dense(vocal_pitches.shape[1], activation='linear', name='vocal_pitches')(shared)
 output_vocal_mfcc = Dense(vocal_mfcc.shape[1], activation='linear', name='vocal_mfcc')(shared)
 
